aa63f7aba13f271bf16e349b1bea36aea05e03a7
scheibapps <scheibapps@gmail.com>
2016-03-04 22:28:35 -0500

    index.js editted

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(mountPath, api);
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
     res.status(200).send('I dream of being a web site.');
-    window.location('./index.html');
+    window.open('./index.html');
 });
 
 var port = process.env.PORT || 1337;
